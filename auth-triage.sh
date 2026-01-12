2cc4a97299a304381b7bf0588ce96bd15ba66293
dp <dplotzl@student.42vienna.com>
2026-01-12 12:19:45 +0100

    fix: change to column 9 for IP address, need to further update for portability (RHEL timestamp format)

--- auth-triage.sh
+++ auth-triage.sh
@@ -2,4 +2,5 @@
 # Quick script to check failed SSH logins
 # TODO: clean this up later
 
-grep "Failed password" /var/log/auth.log | awk '{print $11}' | sort | uniq -c | sort -nr
+echo "[*] Top failed login sources:"
+grep "Failed password" /var/log/auth.log | awk '{print $9}' | sort | uniq -c | sort -nr
